a874bb28e25c859fc374d95cffc0a12c877a5fc0
Abdullah <hocuaf>
2025-11-19 10:07:46 +0300

    Initial Commit

---
+++ breath/breathApp.swift
@@ -0,0 +1,17 @@
+//
+//  breathApp.swift
+//  breath
+//
+//  Created by Abdullah Fat'h HOCU on 19.11.2025.
+//
+
+import SwiftUI
+
+@main
+struct breathApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ breathTests/breathTests.swift
@@ -0,0 +1,17 @@
+//
+//  breathTests.swift
+//  breathTests
+//
+//  Created by Abdullah Fat'h HOCU on 19.11.2025.
+//
+
+import Testing
+@testable import breath
+
+struct breathTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
